4a87ae07bab9bc176e946e8f24f109aec87e3d6c
DESKTOP-51379U5\DELL <onkarpawar25@gmail.com>
2023-06-06 20:40:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/Button";
+import "./App.css"
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count <= 0 ? count : count-1);
+  };
+
+  let resetCount = () => {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Count: {count}</h1>
+      <div className="buttons">
+        <Button className="btn-primary" title={"Decrement"} action={decrementCount} />
+        <Button className="btn-success" title={"Increment"} action={incrementCount} />
+        <Button className="btn-danger" title={"Reset"} action={resetCount} />
+      </div>
     </div>
   );
 }
